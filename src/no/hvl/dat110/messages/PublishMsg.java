b03b6b92a278091972b1142b56963f962de49528
Ehrensverd <mail.eskil@gmail.com>
2021-03-08 15:33:30 +0100

    Add PublishMsg class

--- src/no/hvl/dat110/messages/PublishMsg.java
+++ src/no/hvl/dat110/messages/PublishMsg.java
@@ -11,9 +11,36 @@ public class PublishMsg extends Message {
 
 	// Constructor, get/set-methods, and toString method
 	// as described in the project text
-	
+
+
+	private String topic;
+	private String message;
+
+	public PublishMsg(String user, String topic, String message){
+		super(MessageType.PUBLISH, user);
+		this.topic = topic;
+		this.message = message;
+
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
 	public String getMessage() {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "Publish Msg" + "topic='" + topic + ", message='" + message + super.toString();
 	}
 }
