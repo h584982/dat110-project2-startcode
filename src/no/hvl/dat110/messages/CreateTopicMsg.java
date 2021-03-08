a1e84538d6184d0eacba3231c6b0e096b72305c1
thomaseikhaug <thomaseikhaug@thomas-mbp.home>
2021-03-08 15:10:27 +0100

    CreateTopicMsg ferdig

--- src/no/hvl/dat110/messages/CreateTopicMsg.java
+++ src/no/hvl/dat110/messages/CreateTopicMsg.java
@@ -1,28 +1,33 @@
 package no.hvl.dat110.messages;
 
 public class CreateTopicMsg extends Message {
-	
+
 	// message sent from client to create topic on the broker
-	
-	// TODO: 
+
+	// TODO:
 	// Implement object variables - a topic is required
-	
+
 	// Constructor, get/set-methods, and toString method
-    // as described in the project text	
-	
-	String topic;
-	
-	
-	public CreateTopicMsg(String topic, String user ) {
+	// as described in the project text
+
+	private String topic;
+
+	public CreateTopicMsg(String topic, String user) {
 		super(MessageType.CREATETOPIC, user);
-		this.topic=topic;
+		this.topic = topic;
 	}
-	
-	
-	
-/*	@Override
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	@Override
 	public String toString() {
-		return ;
-	};
-*/	
+		return "CreateTopicMsg [topic=" + topic + super.toString() + "]";
+	}
+
 }
